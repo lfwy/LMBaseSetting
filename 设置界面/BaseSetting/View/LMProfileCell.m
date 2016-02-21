830f849ad901a0e9ea61dbb3400f40bc567d41ff
lim <495247532@qq.com>
2016-02-21 19:40:19 +0800

    第一次提交，非xcode工程，可拖入工程中直接继承使用

---
+++ 设置界面/BaseSetting/View/LMProfileCell.m
@@ -0,0 +1,26 @@
+//
+//  LMProfileCell.m
+//  我界面的单元格
+//
+//  Created by lim on 15/8/7.
+//  Copyright © 2015年 lim. All rights reserved.
+//
+
+#import "LMProfileCell.h"
+
+@implementation LMProfileCell
+
+- (instancetype)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier {
+    if (self = [super initWithStyle:style reuseIdentifier:reuseIdentifier]) {
+        self.detailTextLabel.font = [UIFont systemFontOfSize:12];
+    }
+    return self;
+}
+
+- (void)layoutSubviews {
+    [super layoutSubviews];
+    
+    self.detailTextLabel.x = CGRectGetMaxX(self.textLabel.frame) + 5;
+}
+
+@end
